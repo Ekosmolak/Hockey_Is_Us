3209594946f9cef940e38b4b922cc51ba0e767c2
Ekosmolak <ekosmolak@gmail.com>
2024-12-04 17:16:29 -0600

    Select drop down works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,20 @@
 class ProductsController < InheritedResources::Base
   def index
-    if params[:equipment_type].present?
-      category_id = Category.find_by(equipment_type: params[:id])
-      @products = Product.where(category_id: category_id) if category_id
-    else
-      @products = Product.all
-    end
+    @products = Product.all
   end
 
   def search
+    @products = Product.all
+
     if params[:keywords].present?
-      @products = Product.joins(:brand).where("products.model LIKE ? OR brands.name LIKE ?", "%#{params[:keywords]}%", "%#{params[:keywords]}%")
-    else
-      @products = Product.none
+      @products = @products.joins(:brand).where("products.model LIKE ? OR brands.name LIKE ?", "%#{params[:keywords]}%", "%#{params[:keywords]}%")
     end
+
+    if params[:equipment_type].present?
+      category_id = Category.find_by(equipment_type: params[:equipment_type])&.id
+      @products = @products.where(category_id: category_id) if category_id
+    end
+
     render :index
   end
 
