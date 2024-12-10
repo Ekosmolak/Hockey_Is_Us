7e87b6a29e8411393c68b0f8bcafcec2a2124394
Ekosmolak <ekosmolak@gmail.com>
2024-12-10 03:01:24 -0600

    devise has been causing 401 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  before_action :authenticate_customer!
   protect_from_forgery with: :exception
 
-  before_action :authenticate_customer!
   before_action :initialize_session
 
   helper_method :cart
@@ -16,7 +15,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || root_path
+    root_path
   end
 
   # Use Devise's built-in helpers
